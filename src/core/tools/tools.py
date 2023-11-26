1afbb0e9fb57fede9584ded400617c0d831ac8fc
Domin <domin.dober@gmail.com>
2023-11-26 18:26:39 +0100

    Rewrite todoist tool to class based tool

--- src/core/tools/tools.py
+++ src/core/tools/tools.py
@@ -1,12 +1,8 @@
-from typing import List
-
 from langchain.agents import tool
 from langchain.tools import YouTubeSearchTool
-from todoist_api_python.models import Task
 
 from src.modules.light.light import Light, LightState
-from src.modules.todoist.consts import Tag
-from src.modules.todoist.todoist import Todoist
+from src.modules.todoist.tools import AddTodoTasksTool
 from src.modules.user_communication.communication import UserCommunication
 from src.modules.youtube.youtube import YoutubeConnector
 
@@ -25,22 +21,6 @@ def play_video(url: str):
     return True
 
 
-todoist = Todoist()
-
-
-@tool
-def add_todo_task(task_name: str, tag: Tag, due_string: str = "today") -> Task:
-    """Adds task to list. Due_string is human-like string. Returns data about added task."""
-    return todoist.add_task(task_name, tag, due_string)
-
-
-@tool
-def add_todo_tasks(tasks_names: List[str], tag: Tag, due_string: str = "today") -> bool:
-    """Adds many tasks at once with the same tag. Due_string is human-like string."""
-    todoist.add_tasks(tasks_names, tag, due_string)
-    return True
-
-
 # google_search = GoogleSearch()
 
 
@@ -55,8 +35,7 @@ TOOLS = [
     respond_to_user,
     YouTubeSearchTool(),
     play_video,
-    add_todo_task,
-    add_todo_tasks,
+    AddTodoTasksTool(),
     switch_light,
 ]
 #
